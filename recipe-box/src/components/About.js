916d5e300ec5be02ef9c5e7596d4dcbdff0bf9e6
Kevin Duggan <kevinduggan95@gmail.com>
2023-03-09 16:09:34 -0600

    about section linked up with info and routes

--- recipe-box/src/components/About.js
+++ recipe-box/src/components/About.js
@@ -3,7 +3,49 @@ function About(){
 
 
     return (
-        <div></div>
+        <div><h3>Check Out The Creators:</h3>
+        <div id="social">
+            <div id="grace">
+                <figure>
+                    <img
+                        src="https://images.squarespace-cdn.com/content/v1/53c129c4e4b0c9cf4d63fe7b/1480331789348-38MXDDJ4C5HYWQ7H4NQG/peaches.gif"
+                        className="gif"
+                    />
+                    <figcaption>
+                        <h3>Grace</h3>
+                    </figcaption>
+                </figure>
+                <a href="https://www.linkedin.com/in/gracenieboer/"><i class="linkedin icon"></i></a>
+                <a href="https://github.com/gnieb"><i class="github icon"></i></a>
+            </div>
+
+            <div id="taylor">
+                <figure>
+                    <img
+                        src="https://images.squarespace-cdn.com/content/v1/53c129c4e4b0c9cf4d63fe7b/1480331786333-N2O4YIO3I4HDOEK6WNZ3/bacon.gif"
+                        className="gif"
+                    />
+                    <figcaption>
+                        <h3>Taylor</h3>
+                    </figcaption>
+                </figure>
+                <a href="www.linkedin.com/in/taylorambrose"><i class="linkedin icon"></i></a>
+                <a href="https://github.com/tambrose12"><i class="github icon"></i></a>
+            </div>
+            <div id="duggan">
+                <figure>
+                    <img
+                        className="gif"
+                        src="https://images.squarespace-cdn.com/content/v1/53c129c4e4b0c9cf4d63fe7b/1480331789992-7VWSLO09C8RKDUZ6FK1X/pizzahh.gif"
+                    />
+                    <figcaption>
+                        <h3>Duggan</h3>
+                    </figcaption>
+                </figure>
+                <a href="https://www.linkedin.com/in/kevin-duggan-420624164/"><i class="linkedin icon"></i></a>
+                <a href="https://github.com/Duggan1"><i class="github icon"></i></a>
+            </div>
+        </div></div>
     )
 }
 export default About;
