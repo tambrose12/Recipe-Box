a5343e556b7c023818aa3cf9e24fc1aa4b80c852
Grace Nieboer <grace.nieboer@outlook.com>
2023-03-09 11:55:22 -0500

    added github links to dancing us

--- recipe-box/src/components/Footer.js
+++ recipe-box/src/components/Footer.js
@@ -12,21 +12,27 @@ function Footer() {
                         src="https://images.squarespace-cdn.com/content/v1/53c129c4e4b0c9cf4d63fe7b/1480331786333-N2O4YIO3I4HDOEK6WNZ3/bacon.gif"
                         class="gif"
                     />
-                    <figcaption>Taylor</figcaption>
+                    <figcaption>
+                        <a href ="https://github.com/tambrose12" >Taylor</a>
+                        </figcaption>
                 </figure>
                 <figure>
                     <img
                         src="https://images.squarespace-cdn.com/content/v1/53c129c4e4b0c9cf4d63fe7b/1480331789348-38MXDDJ4C5HYWQ7H4NQG/peaches.gif"
                         className="gif"
                     />
-                    <figcaption>Grace</figcaption>
+                    <figcaption>
+                        <a href="https://github.com/gnieb" >Grace</a>
+                        </figcaption>
                 </figure>
                 <figure>
                     <img
                         className="gif"
                         src="https://images.squarespace-cdn.com/content/v1/53c129c4e4b0c9cf4d63fe7b/1480331789992-7VWSLO09C8RKDUZ6FK1X/pizzahh.gif"
                     />
-                    <figcaption>Duggan</figcaption>
+                    <figcaption>
+                    <a href="https://github.com/Duggan1" >Duggan</a>
+                    </figcaption>
                 </figure>
                 <br />
 
